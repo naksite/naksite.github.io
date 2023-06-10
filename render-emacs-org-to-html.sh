ea750cf99cf76d37de623a4ae6af559fc0b016c5
mkronvold <>
2023-06-10 13:56:54 -0500

    202306101356

--- render-emacs-org-to-html.sh
+++ render-emacs-org-to-html.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# prepend read the docs based theme to the index
-#(echo "#+SETUPFILE: https://raw.githubusercontent.com/mkronvold/mkronvold.github.io/main/theme-readtheorg.setup" ; cat index.org ) > index.org
-
 emacs index.org --batch -Q --load org-render-html-minimal.el -f org-html-export-to-html --kill
 
 ls -l index.html
