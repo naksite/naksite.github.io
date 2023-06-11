c49a2f004013c421090adb5cd23a88cb178c9337
mkronvold <>
2023-06-11 10:35:34 -0500

    202306111035

--- render-emacs-org-to-html.sh
+++ render-emacs-org-to-html.sh
@@ -5,12 +5,20 @@
 # prepend read the docs based theme to the file if not present
 #(echo "#+SETUPFILE: https://raw.githubusercontent.com/mkronvold/mkronvold.github.io/main/theme-readtheorg.setup" ; cat index.org ) > index.org
 
-[ $(grep -c SETUPFILE ${FILE}.org) == 0 ] && (cat header.theme ${FILE}.org > ${FILE}.tmp ; mv ${FILE}.tmp ${FILE}.org)
+echo "Original org file:"
+ls -oh ${FILE}.org | awk '{$1 = ""; $2 = "";} 1'
 
-emacs ${FILE}.org --batch -Q --load org-render-html-minimal.el -f org-html-export-to-html --kill
+if [ $(grep -c SETUPFILE ${FILE}.org) == 0 ]; then
+  mv -v ${FILE}.org ${FILE}.original
+  cat header.theme ${FILE}.original > ${FILE}.org
+  emacs ${FILE}.org --batch -Q --load org-render-html-minimal.el -f org-html-export-to-html --kill 2> /dev/null
+  mv -v ${FILE}.original ${FILE}.org
+else
+  emacs ${FILE}.org --batch -Q --load org-render-html-minimal.el -f org-html-export-to-html --kill 2> /dev/null
+fi
 
-### I guess we can keep this just in case
+### I guess we can keep this
 #rm ${FILE}~
 
-echo "Resultant file:"
+echo "New html file:"
 ls -oh ${FILE}.html | awk '{$1 = ""; $2 = "";} 1'
