4e9853646a85db84e06312faa8185cb9cf73f9e6
mkronvold <124742091+mkronvold@users.noreply.github.com>
2023-09-13 19:52:08 -0500

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,1 +1,14 @@
-rm -rf public/* ; hugo ; git add . ; git commit -m "pregen" ; git push
+if [ ! "${1}" == "" ] ; then
+  rm -rf exampleSite/resources/_gen/*
+  rm -rf exampleSite/public/*
+  rm -rf resources/_gen/*
+  rm -rf public/*
+
+  hugo
+
+  git add .
+  git commit -m "$1"
+  git push
+else
+  echo "Usage: $0 commit_message"
+fi
